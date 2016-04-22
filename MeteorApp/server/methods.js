c07139246fa8dcd99f03aefad7c02d48345d8c53
shahar halutz <shahar.halutz@gmail.com>
2016-04-22 22:47:42 +0200

    do not add Player to battle if already joined, before removing battle - remove associated players

--- MeteorApp/server/methods.js
+++ MeteorApp/server/methods.js
@@ -24,15 +24,21 @@ Meteor.methods({
   },
 
   'removeBattle': function(_id) {
+    // first remove associated Players:
+    Players.remove({battleId: _id});
+
     Battles.remove({_id: _id});
   },
 
   'joinBattle': function(_id) {
-    Players.insert({
-      userId:Meteor.userId(),
-      battleId:_id
-    });
-
-    console.log('joinBattle:  done. Players count: '+Players.find().count());
+    // already joined this battle?
+    const joinedAlready = Players.findOne({userId : Meteor.userId(),battleId:_id});
+    if (!joinedAlready){
+      Players.insert({
+        userId:Meteor.userId(),
+        battleId:_id
+      });
+      console.log('joinBattle:  done. Players count: '+Players.find().count());
+    }
   }
 });
