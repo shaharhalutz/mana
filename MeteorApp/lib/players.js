1b1f72c479befb3ccdad2136a8658bf8bcc7f894
shahar halutz <shahar.halutz@gmail.com>
2016-04-22 22:48:43 +0200

    added Players Schema

--- MeteorApp/lib/players.js
+++ MeteorApp/lib/players.js
@@ -1,1 +1,33 @@
 Players = new Mongo.Collection('players');
+
+PlayersSchema = new SimpleSchema({
+  mp: {
+    type: Number,
+    defaultValue: 0
+  },
+  hp: {
+    type: Number,
+    defaultValue: 0
+  },
+  userId: {
+    type: String
+  },
+  battleId: {
+    type: String
+  },
+  activeSpellIds: {
+    type: [String],
+    defaultValue: [],
+    minCount: 0
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function () {
+      if (this.isInsert) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    }
+  }
+});
