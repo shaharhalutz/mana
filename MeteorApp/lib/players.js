396823874a417c5ce02cd26283ab6f2711c0403f
shahar halutz <shahar.halutz@gmail.com>
2016-04-23 23:22:53 +0200

    server side: added Players collection behaviour (helpers)

--- MeteorApp/lib/players.js
+++ MeteorApp/lib/players.js
@@ -1,4 +1,4 @@
-Players = new Mongo.Collection('players');
+//Players = new Mongo.Collection('players');
 
 PlayersSchema = new SimpleSchema({
   mp: {
@@ -29,5 +29,24 @@ PlayersSchema = new SimpleSchema({
         this.unset();
       }
     }
+  }
+});
+
+// Add behaviour as in (https://github.com/dburles/meteor-collection-helpers):
+
+// create an object with the desired methods to use as prototype
+var player = {
+  userInfo: function () {
+    return Meteor.users.findOne({_id: this.userId});
+  }
+};
+
+Players = new Meteor.Collection("players", {
+  transform: function (doc) {
+    // create a new empty object with pomodoro as it's prototype
+    var newInstance = Object.create(player);
+
+    // copy the data from doc to newInstance and return newInstance
+    return  _.extend(newInstance, doc);
   }
 });
